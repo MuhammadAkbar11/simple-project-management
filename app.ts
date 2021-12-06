02a1ea5ea573593f732b5d02d8b466c6c6c4262f
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-12-06 23:12:26 +0700

    Section 2 - Object Types

--- app.ts
+++ app.ts
@@ -1,18 +1,11 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  // if (typeof n1 !== "number" || typeof n2 !== "number") {
-  //   throw new Error("Incorrect Input");
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+export {};
 
-let number1: number;
-number1 = 5;
-const number2 = 2.8;
-const printResult = true;
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Zurg",
+  age: 20,
+};
 
-add(number1, number2, printResult, "Result is : ");
+console.log(person);
