dfc683452a7f0e7271c7be69ce954c9c828e57d3
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-12-07 13:42:40 +0700

    Array Types

--- app.ts
+++ app.ts
@@ -1,11 +1,18 @@
-export {};
-
 const person: {
   name: string;
   age: number;
+  hobbies: string[];
 } = {
   name: "Zurg",
   age: 20,
+  hobbies: ["Sports", "Cooking", "Gaming"],
 };
 
+let favActivities: string[];
+favActivities = ["Sports"];
+
 console.log(person);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
