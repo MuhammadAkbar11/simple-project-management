aa1583a11b3604f22c1b8c0a02c8d5c91ed3e9fb
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-01-28 14:59:54 +0700

    Section 3 - Using "Watch Mode"

--- app.ts
+++ app.ts
@@ -1,15 +1,1 @@
-let userInput: unknown;
-let userName: string;
-
-userInput = 5;
-userInput = "Noze";
-
-if (typeof userInput === "string") {
-  userName = userInput;
-}
-
-function genereteError(message: string, code: number): never {
-  throw { message: message, statusCode: code };
-}
-
-genereteError("Opss", 500);
+console.log("Time to get started...");
