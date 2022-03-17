04274f81d6ece4ab3ca69023abfb524577bf033f
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-03-17 14:15:44 +0700

    Section 3 - rootDir and outDir

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
+const title = document.createElement("h1");
+title.textContent = "TypeScript Crash Course By Acedemind";
+
 const renderButton = document.createElement("button");
 renderButton.textContent = "Click Here !";
 
+document.body.append(title);
 document.body.append(renderButton);
 
 const button = document.querySelector("button");
