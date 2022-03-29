133823974ab388a31fd9665400801680f23332b7
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-03-29 22:54:46 +0700

    Section 4 - Arrow Functions

--- src/app2.ts
+++ src/app2.ts
@@ -1,6 +1,21 @@
-// let and const
+//# let and const
 const userName = "Baev";
 // userName = "Baevzev";  const cant be changed
 
 let age = 30;
 age = 29; // Can be changed
+
+//# Arraw Functions
+
+// Arraw Functions with multipe expresions
+const add = (a: number, b: number) => {
+  return a + b;
+};
+
+/*
+const add = (a: number, b: number) => return a + b; // Arrow Functions if only have one expresion
+*/
+
+console.log(add(1, 2));
+
+const printOut: (a: number | string) => void = output => console.log(output);
