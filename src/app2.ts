b9cd47b3fdb5c1e907da59dfa8a825e3446d41d3
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-03-29 22:57:06 +0700

    Section 4 - default Function params

--- src/app2.ts
+++ src/app2.ts
@@ -8,14 +8,18 @@ age = 29; // Can be changed
 //# Arraw Functions
 
 // Arraw Functions with multipe expresions
-const add = (a: number, b: number) => {
-  return a + b;
-};
+// const add = (a: number, b: number) => {
+//   return a + b;
+// };
 
 /*
-const add = (a: number, b: number) => return a + b; // Arrow Functions if only have one expresion
+const add = (a: number, b: number) => a + b; // Arrow Functions if only have one expresion
 */
 
 console.log(add(1, 2));
 
 const printOut: (a: number | string) => void = output => console.log(output);
+
+// # Default Function Params
+
+const add = (a: number = 1, b: number = 5) => a + b;
