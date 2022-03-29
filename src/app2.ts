604b69ed576b3c334cc82c1d1dbcba53b7a68708
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-03-29 23:02:10 +0700

    Section 4 - The Spread Operator (...)

--- src/app2.ts
+++ src/app2.ts
@@ -16,10 +16,25 @@ age = 29; // Can be changed
 const add = (a: number, b: number) => a + b; // Arrow Functions if only have one expresion
 */
 
-console.log(add(1, 2));
+// console.log(add(1, 2));
 
 const printOut: (a: number | string) => void = output => console.log(output);
 
 // # Default Function Params
 
 const add = (a: number = 1, b: number = 5) => a + b;
+
+// # Spread Operator
+
+const hobbies = ["Sports", "Reading"];
+
+const activeHobbies = ["Hiking"];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  name: "baev",
+  age: 500,
+};
+
+const baevzev = { ...person, lastname: "zev" };
