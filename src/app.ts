9893bcf9fd2d70a495fa3b2c91d7ef06cf619678
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-04-06 00:49:02 +0700

    Section 7 - Working Constraints

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@
 
 // promise.then(data => console.log(data));
 
-function merge<T extends object, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 const mergedObj = merge(
