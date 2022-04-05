a9469961cb39f50567ec24d77021578c90af6de5
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-04-06 00:45:11 +0700

    Section 7 - Built a Generic Function

--- src/app.ts
+++ src/app.ts
@@ -8,3 +8,13 @@
 // });
 
 // promise.then(data => console.log(data));
+
+function merge<T extends object, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+const mergedObj = merge(
+  { name: "baev", skills: ["ReactJS", "Nodejs"] },
+  { age: 500 }
+);
+
+console.log(mergedObj);
