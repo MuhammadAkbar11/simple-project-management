44ad73c304635fc675a1901dddb96c2bf89b8fd9
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-06-07 05:50:37 +0700

    Section 8 - Diving into Property Decorators

--- src/app.ts
+++ src/app.ts
@@ -35,3 +35,32 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+console.log("//---");
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator");
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid Price - should be positive");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
