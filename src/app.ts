55996f487d0faab02cb0f2c80c5f21fc8d64282e
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-06-07 06:01:50 +0700

    Section 8 - Accessor & Parameter Decorators

--- src/app.ts
+++ src/app.ts
@@ -43,11 +43,37 @@ function Log(target: any, propertyName: string | Symbol) {
   console.log(target, propertyName);
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Acessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Param decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
 class Product {
   @Log
   title: string;
   private _price: number;
 
+  @Log2
   set price(val: number) {
     if (val > 0) {
       this._price = val;
@@ -60,7 +86,9 @@ class Product {
     this.title = t;
     this._price = p;
   }
-  getPriceWithTax(tax: number) {
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     return this._price * (1 + tax);
   }
 }
