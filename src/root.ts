35023ea9b9ed63cc7ffce09b796b0101d9703ef5
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-06-15 21:02:48 +0700

    Section 9  - Getting Started

--- src/root.ts
+++ src/root.ts
@@ -1,14 +1,1 @@
-const title = document.createElement("h1");
-title.textContent = "Understanding TypeScript - Section 8 Decorators";
 
-const input = document.createElement("input");
-input.setAttribute("type", "email");
-input.id = "user-input";
-input.setAttribute("name", "email");
-input.style.cssText = `
-  padding: .4rem .8rem;
-  font-size: 16px;
-`;
-
-document.getElementById("root")?.appendChild(title);
-document.getElementById("root")?.appendChild(input);
