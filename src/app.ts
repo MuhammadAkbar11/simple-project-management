5910fcc3404e50a6b8df8d096474015069c068ae
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-06-16 07:10:13 +0700

    Section 9 -  Interacting with DOM Elements

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,43 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   formElement: HTMLFormElement;
+  titleInputEl: HTMLInputElement;
+  descriptionInputEl: HTMLInputElement;
+  peopleInputEl: HTMLInputElement;
 
   constructor() {
-    this.templateElement = <HTMLTemplateElement>(
-      document.getElementById("project-input")!
-    );
-    this.hostElement = <HTMLDivElement>document.getElementById("root")!;
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+
+    this.hostElement = document.getElementById("root")! as HTMLDivElement;
     const importedNode = document.importNode(
       this.templateElement.content,
       true
     );
     this.formElement = importedNode.firstElementChild as HTMLFormElement;
+
+    this.titleInputEl = this.formElement.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.descriptionInputEl = this.formElement.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+    this.peopleInputEl = this.formElement.querySelector(
+      "#people"
+    )! as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputEl.value);
+  }
+
+  private configure() {
+    this.formElement.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
